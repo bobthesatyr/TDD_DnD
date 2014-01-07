1340372cb6fe11c5fe4b565558752bd9cb497e0b
Robert Anderson <bobthesatyr@gmail.com>
2014-01-07 17:01:54 -0500

    paladins are similiar to Dio.. HOLY DIVER!

---
+++ src/paladin.rb
@@ -0,0 +1,23 @@
+class Paladin < Character
+
+  def initialize(abilities = {str:10, dex:10, con:10, int:10, wis:10, cha:10})
+    super(abilities, 8)
+  end
+
+  def attack(roll, target)
+    self.attack_bonus += 2 if target.alignment == 'Evil'
+    super(roll, target)
+    self.attack_bonus -= 2 if target.alignment == 'Evil'
+  end
+
+  def deal_damage(target, roll, base_damage = 1)
+
+    if target.alignment == 'Evil'
+      self.critical_multiplier = 3
+      base_damage += 2
+    end
+
+    super(target, roll, base_damage)
+  end
+
+end
---
+++ test/paladin_spec.rb
@@ -0,0 +1,28 @@
+describe Paladin do
+
+  before (:each) do
+    subject = Paladin.new
+    @enemy = Character.new
+  end
+
+  it 'has 8 hp per level instead of 5' do
+    subject.hp.should eq 8
+    subject.gain_experience 1000
+    subject.hp.should eq 16
+  end
+
+  it 'should have higher attack and damage vs evil' do
+    @enemy.alignment = 'Evil'
+
+    subject.attack(8, @enemy)
+    subject.xp.should eq 10
+    @enemy.hp.should eq 2
+  end
+
+  it 'triples the damage on crit for evil doers' do
+    @enemy.alignment = 'Evil'
+    subject.attack(20, @enemy)
+    @enemy.hp.should eq -4
+  end
+
+end
